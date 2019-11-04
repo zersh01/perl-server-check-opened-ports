c97ac66e74dd708e5baa242d51e00c7b14f84328
zersh <zersh@mail.ru>
2019-11-04 22:02:07 +0300

    First commit

---
+++ bin/check_ports_server.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+# Вс ноя  3 13:27:42 MSK 2019
+#parhomenko
+
+use feature(say);
+use strict;
+use warnings;
+use FindBin;
+
+# где ищем библиотеки
+use lib "$FindBin::Bin/../lib"; 
+
+use Net::Server::Check;
+
+our $server = Net::Server::Check->new(conf_file => "$FindBin::Bin/../etc/myserver.cfg");
+
+$server->run();
